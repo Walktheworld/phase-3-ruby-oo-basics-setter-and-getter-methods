54d7369694c78f693984380dfac327995bfc9d22
Joe <Walktheworld@me.com>
2022-02-08 14:18:36 -0700

    commit tree b4119d9060b701d0a43d4f430dfcda63f9856478

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    attr_accessor :breed
+      # setter method
+  def name=(name)
+    @name = name
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
+    attr_accessor :job
+# setter method
+    def name=(name)
+    @name = name
+    end
+
+# getter method
+    def name
+    @name
+    end
 
 end
